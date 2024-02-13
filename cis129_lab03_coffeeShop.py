aa7cf14cb591429e2403570be59ee6e85a423cc9
ochogric <158656986+ochogric@users.noreply.github.com>
2024-02-12 21:43:40 -0700

    Create cis129_lab03_coffeeShop.py
    Add cis129_lab03_coffeeShop.py

---
+++ cis129_lab03_coffeeShop.py
@@ -0,0 +1,15 @@
+Ricardos Coffee and Muffin Shop
+Number of coffees bought?
+55
+Number of muffins bought?
+24
+***************************************
+
+***************************************
+Ricardos Coffee and Muffin Shop Receipt
+55 Coffee at $5 each: $ 275.00
+24 Muffins at $4 each: $ 96.00
+6% tax: $ .78
+---------
+Total: $ 371.78
+***************************************
